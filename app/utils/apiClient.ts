e46713b38c340ac0cd92afe5fdad51be40f0631b
meryoung <dev.meryoung@gmail.com>
2023-07-05 12:22:25 +0900

    API 테스트 코드 적용
    - API 인증 관련 로직

--- app/utils/apiClient.ts
+++ app/utils/apiClient.ts
@@ -4,13 +4,14 @@ import apiAuthor from 'app/utils/apiAuthor';
 const apiClient = {
   idCheck(userId: string) {
     const method: string = 'GET';
-    const url: string = `/api/vi/user/${userId}/check`;
+    const url: string = `/api/v1/user/${userId}/check`;
 
     axios
       .get(url, {
         headers: {
           Authorization: apiAuthor(method, url),
         },
+        withCredentials: true,
       })
       .then((res) => console.log(res));
   },
